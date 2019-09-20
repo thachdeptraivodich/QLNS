14bee7b1e97e4ad8b475ba427b29f22b921ce1b3
thachdeptraivodich <tyntyn2904@gmail.com>
2019-09-20 11:32:44 +0700

    test thu

---
+++ caculation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github
+{
+    public class caculation
+    {
+        private int a, b;
+        public caculation(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int execute(string pheptoan)
+        {
+            int kq = 0;
+            switch(pheptoan)
+            {
+                case "+":
+                    kq = this.a + this.b;
+                    break;
+                case "-":
+                    kq = this.a - this.b;
+                    break;
+                case "*":
+                    kq = this.a * this.b;
+                    break;
+                case "/":
+                    if (b == 0)
+                        new DivideByZeroException("Loi so bi chia");
+                    kq = this.a / this.b;
+                    break;
+            }
+            return kq;
+        }
+
+    }
+}
